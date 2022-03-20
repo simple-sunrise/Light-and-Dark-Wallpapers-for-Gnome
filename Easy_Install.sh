e0657999560d389caedcff119c7348657e347bfe
amrosolaiman <amroawad.soliman@mail.polimi.it>
2022-03-20 12:49:12 +0100

    :rocket: Add Compatibility for more strict linux distributions about permissions

--- Easy_Install.sh
+++ Easy_Install.sh
@@ -1,23 +1,41 @@
-#!/bin/bash
+!/bin/bash
 cd ~
 echo "Downloading needed files started"
 git clone https://github.com/amrosolaiman/Light-and-Dark-Wallpapers-for-Gnome-42.git
 cd Light-and-Dark-Wallpapers-for-Gnome-42
 echo "Files downloaded"
-
-if [[ -d /usr/share/backgrounds/Wallpapers ]]
+echo "Preparing .xml files"
+for file in xml/*.xml
+do
+		search="/usr"
+		replace="/$HOME/.local"
+		sed -i "s|$search|$replace|g" "$file"
+done
+echo ".xml files are ready"
+if [[ -d ~/.local/share/backgrounds/Wallpapers ]]
 then
-	sudo rm -r /usr/share/backgrounds/Wallpapers
+ 	rm -r ~/.local/share/backgrounds/Wallpapers
 	echo "Setting up"
 fi
 
+if [[ ! -d ~/.local/share/backgrounds/Wallpapers ]]
+then
+	mkdir ~/.local/share/backgrounds/
+	echo "Backgrounds folder created"
+fi
+
+if [[ ! -d ~/.local/share/gnome-background-properties/ ]]
+then
+	mkdir ~/.local/share/gnome-background-properties/
+fi
+
 echo "Installing wallpapers..."
-sudo cp -r ./Wallpapers/ /usr/share/backgrounds/
-sudo cp ./xml/* /usr/share/gnome-background-properties/
+cp -r ./Wallpapers/ ~/.local/share/backgrounds/
+cp ./xml/* ~/.local/share/gnome-background-properties/
 echo "Dynamic Wallpapers has been installed!"
 cd ~
 echo "Deleting files used only for the installation process"
-sudo rm -r Light-and-Dark-Wallpapers-for-Gnome-42
+rm -r Light-and-Dark-Wallpapers-for-Gnome-42
 echo "    |"
 echo "    '---> Deleted unneeded files!"
 echo "Now, don't forget to set your preferred dynamic wallpaper from Settings!"
