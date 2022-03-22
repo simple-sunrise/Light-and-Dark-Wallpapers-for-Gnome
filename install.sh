7a70b31040e247853f787d9d36db77af7040631f
amrosolaiman <amroawad.soliman@mail.polimi.it>
2022-03-22 17:26:22 +0100

    :wrench: Modify install.sh for local install

--- install.sh
+++ install.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
-if [[ -d /usr/share/backgrounds/Wallpapers ]]
+echo "Preparing .xml files"
+for file in xml/*.xml
+do
+		search="/usr"
+		replace="/$HOME/.local"
+		sed -i "s|$search|$replace|g" "$file"
+done
+echo ".xml files are ready"
+if [[ -d ~/.local/share/backgrounds/Wallpapers ]]
 then
-	sudo rm -r /usr/share/backgrounds/Wallpapers
-	echo "Cleaning up"
+ 	rm -r ~/.local/share/backgrounds/Wallpapers
+	echo "Setting up"
 fi
 
-#if [[ -d /usr/share/gnome-background-properties ]]
-#then
-#	sudo rm -r /usr/share/gnome-background-properties
-#	sudo mkdir /usr/share/gnome-background-properties
-#fi
+if [[ ! -d ~/.local/share/backgrounds/ ]]
+then
+	mkdir ~/.local/share/backgrounds/
+	echo "Backgrounds folder created"
+fi
+
+if [[ ! -d ~/.local/share/gnome-background-properties/ ]]
+then
+	mkdir ~/.local/share/gnome-background-properties/
+	echo "gnome-background-properties folder created"
+fi
 
 echo "Installing wallpapers..."
-sudo cp -r ./Wallpapers/ /usr/share/backgrounds/
-sudo cp ./xml/* /usr/share/gnome-background-properties/
-echo "Wallpapers has been installed. Enjoy setting them as your desktop background!"
+cp -r ./Wallpapers/ ~/.local/share/backgrounds/
+cp ./xml/* ~/.local/share/gnome-background-properties/
+echo "Wallpapers has been installed!"
